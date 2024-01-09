90db7daf62d2a5b44ba90d54d81955225af0321b
InFinity_54 <corentinkreicher@gmail.com>
2024-01-09 17:05:35 +0100

    Suite et fin des actions liées au comportement (CP4 à CP13)

--- LOSPAÉ/Views/MainPage.xaml.cs
+++ LOSPAÉ/Views/MainPage.xaml.cs
@@ -68,4 +68,54 @@ public sealed partial class MainPage : Page
     {
         PerformStudentNoteChange(0.00, 2.00, "CP3", "Envoi d'un objet dans la tête d'un collègue");
     }
+
+    public void CP4_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 5.00, "CP4", "Insolence envers le professeur");
+    }
+
+    public void CP5_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 20.00, "CP5", "Préparation et consommation d'alcool en cours");
+    }
+
+    public void CP6_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 20.00, "CP6", "Préparation et consommation de drogue en cours");
+    }
+
+    public void CP7_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 2.00, "CP7", "Utilisation du téléphone portable sans autorisation");
+    }
+
+    public void CP8_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 3.00, "CP8", "Ne pas se tenir correctement en cours");
+    }
+
+    public void CP9_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 1.50, "CP9", "Écouter de la musique trop fort");
+    }
+
+    public void CP10_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 2.00, "CP10", "Écouter de la musique sur un haut-parleur sans autorisation (PC, téléphone, enceinte…)");
+    }
+
+    public void CP11_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 10.00, "CP11", "Partir pendant une récréation sans y être autorisé");
+    }
+
+    public void CP12_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 20.00, "CP12", "Sécher un cours");
+    }
+
+    public void CP13_Click(object sender, RoutedEventArgs e)
+    {
+        PerformStudentNoteChange(0.00, 5.00, "CP11", "Frapper un collègue");
+    }
 }
