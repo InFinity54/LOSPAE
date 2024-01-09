fe577c23c5aa91b064d0beebff95285ce85e9f48
InFinity_54 <corentinkreicher@gmail.com>
2024-01-09 15:22:45 +0100

    Importation du CSV dans les paramètres (front uniquement)

--- LOSPAÉ/Views/SettingsPage.xaml.cs
+++ LOSPAÉ/Views/SettingsPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using LOSPAÉ.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.ApplicationModel.Store;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace LOSPAÉ.Views;
 
@@ -15,5 +18,39 @@ public sealed partial class SettingsPage : Page
     {
         ViewModel = App.GetService<SettingsViewModel>();
         InitializeComponent();
+    }
+
+    private async void CsvDataImport_Click(object sender, RoutedEventArgs e)
+    {
+        CsvDataImport.IsEnabled = false;
+        CsvDataImportProgress.IsActive = true;
+
+        var picker = new FileOpenPicker();
+        picker.ViewMode = PickerViewMode.Thumbnail;
+        picker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+        picker.FileTypeFilter.Add(".csv");
+
+        var WindowHandle = WindowNative.GetWindowHandle(App.MainWindow);
+        InitializeWithWindow.Initialize(picker, WindowHandle);
+
+        Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+        if (file != null)
+        {
+            ContentDialog dialog = new ContentDialog();
+
+            // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+            dialog.XamlRoot = this.XamlRoot;
+            dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+            dialog.Title = "Fichier sélectionné";
+            dialog.PrimaryButtonText = "OK";
+            dialog.DefaultButton = ContentDialogButton.Primary;
+            dialog.Content = file.Name;
+
+            await dialog.ShowAsync();
+        }
+
+        CsvDataImport.IsEnabled = true;
+        CsvDataImportProgress.IsActive = false;
     }
 }
