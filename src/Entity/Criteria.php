32639cac0dd153f0ff49d5436eac81456fe3ffca
Corentin KREICHER <contact@infinity54.fr>
2024-02-26 19:04:12 +0100

    Gestion des critères de notation

---
+++ src/Entity/Criteria.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CriteriaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CriteriaRepository::class)]
+class Criteria
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $impact = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $modality = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImpact(): ?float
+    {
+        return $this->impact;
+    }
+
+    public function setImpact(float $impact): static
+    {
+        $this->impact = $impact;
+
+        return $this;
+    }
+
+    public function getModality(): ?string
+    {
+        return $this->modality;
+    }
+
+    public function setModality(?string $modality): static
+    {
+        $this->modality = $modality;
+
+        return $this;
+    }
+}
