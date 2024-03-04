cbacf99a4e95d056cd5fa8865ef247d9c70574ed
InFinity_54 <corentinkreicher@gmail.com>
2024-03-04 16:37:58 +0100

    Accueil pour enseignants (incomplet)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         if (!is_null($this->getUser()) && !$this->getUser()->isIsActivated()) {
             return $this->redirectToRoute("deactivated");
@@ -17,6 +19,23 @@ class HomeController extends AbstractController
 
         if (is_null($this->getUser())) {
             return $this->redirectToRoute("login");
+        }
+
+        if (in_array("ROLE_TEACHER", $this->getUser()->getRoles())) {
+            $students = $entityManager->getRepository(User::class)->findUsersByRole("ROLE_STUDENT");
+            $studentsNotesSum = 0;
+
+            foreach ($students as $student) {
+                if (!is_null($student->getNote())) {
+                    $studentsNotesSum += $student->getNote()->getCurrentNote();
+                }
+            }
+
+            return $this->render('pages/logged_in/index_teacher.html.twig', [
+                "studentsCount" => count($students),
+                "studentsAverageNote" => $studentsNotesSum / count($students),
+                "studentsNotesChangeEventsCount" => 0
+            ]);
         }
 
         return $this->render('pages/logged_in/index.html.twig');
