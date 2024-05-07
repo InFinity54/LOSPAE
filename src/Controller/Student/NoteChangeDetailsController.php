4c29433e6e033454b5b1fa2db4d07c93bedc6f5a
InFinity_54 <corentinkreicher@gmail.com>
2024-05-07 23:25:55 +0200

    Ajout d'une fonctionnalité de bilan de points pour les étudiants

---
+++ src/Controller/Student/NoteChangeDetailsController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller\Student;
+
+use App\Entity\Criteria;
+use App\Entity\NoteChange;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class NoteChangeDetailsController extends AbstractController
+{
+    #[Route('/details', name: 'student_details')]
+    public function history(EntityManagerInterface $entityManager): Response
+    {
+        if (!is_null($this->getUser()) && !$this->getUser()->isIsActivated()) {
+            return $this->redirectToRoute("deactivated");
+        }
+
+        if (is_null($this->getUser())) {
+            return $this->redirectToRoute("login");
+        }
+
+        if (!in_array("ROLE_STUDENT", $this->getUser()->getRoles())) {
+            $this->addFlash("danger", "Vous n'êtes pas autorisé à accéder à cette page.");
+            return $this->redirectToRoute("homepage");
+        }
+        
+        $student = $entityManager->getRepository(User::class)->findOneBy(["email" => $this->getUser()->getUserIdentifier()]);
+        $criterias = $entityManager->getRepository(Criteria::class)->findBy([], ["name" => "ASC"]);
+        $criteriasCount = [];
+        $noteChanges = $entityManager->getRepository(NoteChange::class)->findBy(["student" => $student->getId()], ["occuredAt" => "DESC"]);
+        $totalAddedPoints = 0.0;
+        $totalRemovedPoints = 0.0;
+
+        foreach ($criterias as $criteria) {
+            $criteriasCount[$criteria->getId()] = 0;
+        }
+
+        foreach ($noteChanges as $noteChange) {
+            $criteriasCount[$noteChange->getCriteria()->getId()]++;
+
+            if ($noteChange->getImpact() > 0) {
+                $totalAddedPoints += $noteChange->getImpact();
+            } else {
+                $totalRemovedPoints += ($noteChange->getImpact() * -1);
+            }
+        }
+
+        return $this->render('pages/logged_in/student/details.html.twig', [
+            "totalAddedPoints" => $totalAddedPoints,
+            "totalRemovedPoints" => $totalRemovedPoints,
+            "criterias" => $criterias,
+            "criteriasCount" => $criteriasCount
+        ]);
+    }
+}
