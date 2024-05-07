629bf0ee29da62514adc2513bb78138845db8772
InFinity_54 <corentinkreicher@gmail.com>
2024-05-07 22:45:51 +0200

    Ajout d'un page de changelog

---
+++ src/Controller/ChangelogController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ChangelogController extends AbstractController
+{
+    #[Route('/changelog', name: 'changelog')]
+    public function changelog(): Response
+    {
+        if (!is_null($this->getUser()) && !$this->getUser()->isIsActivated()) {
+            return $this->redirectToRoute("deactivated");
+        }
+
+        if (is_null($this->getUser())) {
+            return $this->redirectToRoute("login");
+        }
+
+        return $this->render('pages/logged_in/changelog.html.twig');
+    }
+}
