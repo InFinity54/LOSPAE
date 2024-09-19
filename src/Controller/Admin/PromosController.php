1add6490df6f177ac880055022bd7521cc0afe66
InFinity_54 <corentinkreicher@gmail.com>
2024-09-19 13:38:14 +0200

    Liste des promotions et ajout d'une nouvelle promotion

--- src/Controller/Admin/PromosController.php
+++ src/Controller/Admin/PromosController.php
@@ -6,7 +6,6 @@ use App\Entity\Promo;
 use App\Entity\School;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -40,6 +39,40 @@ class PromosController extends AbstractController
 
         return $this->render('pages/logged_in/admin/promos.html.twig', [
             "promos" => $promos
+        ]);
+    }
+
+    #[Route('/admin/promos/add', name: 'admin_promo_add')]
+    public function promoAdd(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!is_null($this->getUser()) && !$this->getUser()->isIsActivated()) {
+            return $this->redirectToRoute("deactivated");
+        }
+
+        if (is_null($this->getUser())) {
+            return $this->redirectToRoute("login");
+        }
+
+        if (!in_array("ROLE_ADMIN", $this->getUser()->getRoles())) {
+            $this->addFlash("danger", "Vous n'êtes pas autorisé à accéder à cette page.");
+            return $this->redirectToRoute("homepage");
+        }
+
+        if ($request->request->count() > 0) {
+            $promo = new Promo();
+            $promo->setName($request->request->get("name"));
+            $promo->setSchool($entityManager->getRepository(School::class)->find($request->request->get("school")));
+            $entityManager->persist($promo);
+            $entityManager->flush();
+
+            $this->addFlash("success", "La promotion a été enregistré.");
+            return $this->redirectToRoute("admin_promos");
+        }
+
+        $schools = $entityManager->getRepository(School::class)->findBy([], ["name" => "ASC"]);
+
+        return $this->render('pages/logged_in/admin/promo_add.html.twig', [
+            "schools" => $schools
         ]);
     }
 }
