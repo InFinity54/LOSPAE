bc1f697c06ac68b8f1963206aaa5ffba2431290c
InFinity_54 <corentinkreicher@gmail.com>
2024-10-06 17:29:11 +0200

    Ajout de l'import d'enseignants

---
+++ public/assets/js/pages/logged_in/admin/teachers.js
@@ -0,0 +1,33 @@
+$("#teacherglobalselection").change((event) => {
+    if($("#teacherglobalselection").is(":checked")) {
+        $(".teacherselection").prop("checked", true);
+    } else {
+        $(".teacherselection").prop("checked", false);
+    }
+})
+
+$("#configure_selected_teachers").on("click", () => {
+    handleAction("edit");
+});
+
+$("#enable_selected_teachers").on("click", () => {
+    handleAction("enable");
+});
+
+$("#disable_selected_teachers").on("click", () => {
+    handleAction("disable");
+});
+
+$("#delete_selected_teachers").on("click", () => {
+    handleAction("remove");
+});
+
+function handleAction(actionName) {
+    let ids = [];
+
+    $(".teacherselection:checked").each(function () {
+        ids.push($(this).data("teacher"));
+    });
+
+    window.location.href = window.location.origin + "/admin/teachers/" + actionName + "/" + ids.join(",");
+}
